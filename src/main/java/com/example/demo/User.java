ebc80fd3e0c13cae60cb9f6f7e0ba4dc828caab3
DESKTOP-LJ5QNTR\user <jeongminb933@gmail.com>
2024-11-26 18:50:12 +0900

    프로젝트 초기설정 유저컨트롤러 유저리포짓토리 유저엔티티 에러페이지 생성프로젝트 초기설정 유저컨트롤러 유저리포짓토리 유저엔티티 에러페이지 생성

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+/*    @Column(unique = true, nullable = false)*/
+    private String username;
+
+/*    @Column(unique = true, nullable = false)*/
+    private String email;
+
+/*    @Column(nullable = false)*/
+    private String password;
+
+/*    @Column(nullable = false, updatable = false)*/
+/*    private LocalDateTime created_at;*/
+
+/*    @PrePersist
+    protected void onCreate() {
+        this.created_at = LocalDateTime.now();
+    }*/
+}
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor //이 어노테이션 써야 DB 불러올 수 있음
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/signup")
+    String signup() {
+        return "signup.html";
+    }
+
+    @PostMapping("/addUserStatus")
+    String addUserStatus(@RequestParam(name ="username")
+                         String username,@RequestParam(name="email")
+                        String email, @RequestParam(name="password") String password){
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        System.out.println(user.getUsername()+user.getEmail()+user.getPassword());
+        userRepository.save(user);
+        return "redirect:/signup";
+    }
+
+    @GetMapping("/login")
+    String login(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("user", new User());
+
+        return "login.html";
+    }
+
+}
