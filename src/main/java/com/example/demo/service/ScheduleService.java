7e7e0518170ac5e77d4f2e2852fc137abaa1ed60
agree0002 <donghui1365@outlook.kr>
2024-12-19 16:35:23 +0900

    v

--- src/main/java/com/example/demo/service/ScheduleService.java
+++ src/main/java/com/example/demo/service/ScheduleService.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.Schedule;
 import com.example.demo.repository.ScheduleRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 
 
 @Transactional
+@Service
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
