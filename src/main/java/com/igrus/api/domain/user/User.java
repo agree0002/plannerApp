66f514ecdda0b876b6ed17ade229ac181e549994
agree0002 <donghui1365@outlook.kr>
2025-07-12 22:38:32 +0900

    회원가입 기능 추가

---
+++ src/main/java/com/igrus/api/domain/user/User.java
@@ -0,0 +1,27 @@
+package com.igrus.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
